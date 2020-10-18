5053b0f68763fc95b76e556e0ceba01243fce584
Arlandis Word <arlandis@remind101.com>
2020-10-18 11:45:54 -0400

    complete rework; use leb128 encoding, much better

--- spec/varint_spec.rb
+++ spec/varint_spec.rb
@@ -1,65 +1,10 @@
 require_relative '../varint'
 
 RSpec.describe "varint" do
-# Examples taken from the MIDI File format specification table here:
-# https://en.wikipedia.org/wiki/Variable-length_quantity
-  let(:examples) {
-    [
-      [0, ['0x00']],
-      [127, ['0x7f']],
-      [128, ['0x81', '0x00']],
-      [8192, ['0xc0', '0x00']],
-      [16383, ['0xff', '0x7f']],
-      [16384, ['0x81', '0x80', '0x00']],
-      [2097151, ['0xff', '0xff', '0x7f']],
-      [2097152, ['0x81', '0x80', '0x80', '0x00']],
-      [134217728, ['0xc0', '0x80', '0x80', '0x00']],
-      [268435455, ['0xff', '0xff', '0xff', '0x7f']]
-    ]
-  }
-
-  context 'helper functions' do
-    describe 'another_byte_follows?' do
-      it "returns true when the leading bit is flipped" do
-        expect(another_byte_follows?('0xff')).to eq(true)
-      end
-
-      it "returns true when the leading bit is off" do
-        expect(another_byte_follows?('0x0f')).to eq(false)
-      end
-    end
-
-    describe 'binary_to_decimal' do
-      it "computes correctly" do
-        expect(binary_to_decimal('00000001')).to eq(1)
-        expect(binary_to_decimal('00000011')).to eq(3)
-        expect(binary_to_decimal('00001111')).to eq(15)
-        expect(binary_to_decimal('000000011111010000000000')).to eq(128_000)
-      end
-    end
-
-    describe 'decode_byte_to_binary' do
-      it "strips the leading bit and converts the byte" do
-        expect(decode_byte_to_binary('0x80')).to eq('0000000')
-      end
-    end
-  end
-
-  describe "encode" do
+  describe "encode and decode" do
     it "examples work" do
-      examples.each do |example|
-        encoded = encode(example[0])
-        expect(encoded).to eq(example[1])
-      end
-    end
-  end
-
-  describe "decode" do
-    it "examples decode to themselves" do
-      examples.each do |example|
-        encoded = encode(example[0])
-        decoded = decode(encoded)
-        expect(decoded).to eq(example[0])
+      (-1_000_000..1_000_000).each do |example|
+        expect(decode(encode(example))).to eq(example)
       end
     end
   end
--- varint.rb
+++ varint.rb
@@ -1,62 +1,36 @@
-def binary_to_decimal(bin)
-  length = bin.length
-  forward_index = 0
-  backward_index = bin.length - 1
-  acc = 0
-  while forward_index < length do
-    bit = bin[forward_index]
-    acc += 2 ** backward_index if bit == "1"
-    forward_index += 1
-    backward_index -= 1
-  end
-  acc
-end
-
-def pad_with_zeroes(str, desired_len)
-  str.rjust(desired_len, '0')
-end
+SIGN_MASK = 0x40
+TRAILING_7_MASK = 0x7f
+HIGH_ORDER_BIT_MASK = 0x80
 
 def encode(x)
-  bit_form = x.to_s(2)
-  seven_bit_groupings = bit_form.split("").reverse.each_slice(7).to_a.reverse.map do |chunk|
-    chunk.reverse.join("").rjust(7, "0")
-  end
-  least_sig_byte = seven_bit_groupings[-1]
-  least_sig_byte.insert(0, "0")
-  leading_bytes = seven_bit_groupings[0...seven_bit_groupings.length - 1]
-  leading_bytes.each do |byte|
-    byte.insert(0, "1")
-  end
-  seven_bit_groupings.map do |grouping|
-    "0x%02x" % grouping.to_i(2)
-  end.flatten
-end
+  bytes = []
+  acc = x
+  loop do
+    byte = acc & TRAILING_7_MASK
+    acc = acc >> 7
+    sign_bit = (byte & SIGN_MASK)
+    if (acc == 0 && sign_bit == 0) || (acc == -1 && sign_bit != 0)
+      bytes << byte # last byte needs no mask
+      return bytes
+    end
 
-def another_byte_follows?(byte)
-  as_hex = byte.hex
-  msb = as_hex & 0x80
-  msb != 0
-end
-
-# Strips the most significant bit
-# as that was only used for the encoding
-# scheme
-def decode_byte_to_binary(byte)
-  pad_with_zeroes(byte.hex.to_s(2), 8)[1..-1]
+    bytes << (byte | HIGH_ORDER_BIT_MASK)
+  end
 end
 
 def decode(bytes)
-  full_bit_arr = []
-  idx = 0
-  bytes.each do |byte|
-    bit_str = decode_byte_to_binary(byte)
-    full_bit_arr << bit_str
-    break unless another_byte_follows?(byte)
+  num = 0
+  shift = 0
+  bytes.each_with_index do |byte, index|
+    original = byte & TRAILING_7_MASK
+    # shift the bits in this byte back to their original ordering, then add them
+    num = num | (original << shift)
+    shift += 7
+    if (index == bytes.length - 1) && (byte & SIGN_MASK) != 0
+      # dealing with a negative number, so sign extend
+      num = num | (-1 << shift)
+    end
   end
-
-  binary_to_decimal(
-    pad_with_zeroes(
-      full_bit_arr.join(""), bytes.length * 8
-    )
-  )
+  num
 end
+
